73f1b01cf239b4ccbceb9d0af5e0be66af1cb6aa
arkt0956 <55053131+arkt0956@users.noreply.github.com>
2022-11-01 20:26:34 +0900

    pass-parameter 수신 기능 구현

--- app.py
+++ app.py
@@ -1,51 +1,42 @@
-from flask import Flask, jsonify, render_template, redirect
+from urllib import response
+from flask import Flask, jsonify, render_template, redirect, request
 from requests import get
 from pymongo import MongoClient
 
 app = Flask(__name__)
-client = MongoClient()
+client = MongoClient('mongodb+srv://test:test@cluster0.lrizo6r.mongodb.net/?retryWrites=true&w=majority')
 # 보안 이슈
 
 db = client.dblod
 
 
-@ app.route('/')
+@app.route("/")
 def home():
     return render_template('index.html')
 
 
-@ app.route('/visit/<int:id>', methods=["GET"])
-def comment(id):
+@app.route("/visit/<member_id>", methods=["GET"])
+def comment(member_id):
     status = ""
-    data = list(db.visited.find({'member_id': id}, {'_id': False}))
-    code = get(f"/visit/{id}").status_code
-    # print(data)
-    if code >= 500:
-        status = "5xx / server error"
-    elif code >= 400:
-        status = "4xx / client error"
-    elif code >= 300:
-        status = "3xx / redirection"
-    elif code >= 200:
-        status = "2xx / successful"
-    elif code >= 100:
-        status = "1xx / informational response"
+    data = list(db.visited.find({'member_id': int(member_id)}, {'_id': False}))
 
     return jsonify({'status': status, 'data': data})
 
-@app.route("/visit/add/<int:member_id>", methods=["POST"])
+
+@app.route("/visit/add/<member_id>", methods=["POST"])
 def visited_post(member_id):
     visited_id_receive = request.form['visited_id_give']
     visited_comment_receive = request.form['visited_comment_give']
     doc = {
         'id': visited_id_receive,
         'comment': visited_comment_receive,
-        'member_id': member_id
+        'member_id': int(member_id)
     }
     db.visited.insert_one(doc)
     return jsonify({'msg': '등록 완료!'})
 
-@ app.route('/home')
+
+@app.route('/home')
 def back():
     return redirect("/")
 
